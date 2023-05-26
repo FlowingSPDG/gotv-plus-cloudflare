e41b17c666297540b6479c2477ff92d8a5c27869
Shugo Kawamura <shugo_k@flowing.tokyo>
2023-05-26 18:55:26 +0900

    Fragmentを登録出来るように

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,68 @@
 use worker::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[durable_object]
+pub struct Sync {
+    tick: u32,
+    rtdelay: u8,
+    rcvage: u8,
+    fragment: u32,
+    signup_fragment: u32,
+    tps: u8,
+    protocol: u8, // should be 4
+}
+
+#[durable_object]
+impl DurableObject for Sync {
+    fn new(state: State, _env: Env) -> Self {
+        Self {
+            tick: 0,
+            rtdelay: 0,
+            rcvage: 0,
+            fragment: 0,
+            signup_fragment: 0,
+            tps: 0,
+            protocol: 0,
+        }
+    }
+
+    async fn fetch(&mut self, _req: Request) -> Result<Response> {
+        Response::ok(serde_json::to_string(self)?)
+    }
+}
+
 
 #[event(fetch, respond_with_errors)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     let router = Router::new();
 
     router
-        .on_async("/durable", |_req, ctx| async move {
-            let namespace = ctx.durable_object("CHATROOM")?;
-            let stub = namespace.id_from_name("A")?.get_stub()?;
-            stub.fetch_with_str("/messages").await
-        })
-        .get("/secret", |_req, ctx| {
-            Response::ok(ctx.secret("CF_API_TOKEN")?.to_string())
-        })
-        .get("/var", |_req, ctx| {
-            Response::ok(ctx.var("BUILD_NUMBER")?.to_string())
-        })
-        .post_async("/kv", |_req, ctx| async move {
-            let kv = ctx.kv("SOME_NAMESPACE")?;
+        // Request from CS:GO Server
 
-            kv.put("key", "value")?.execute().await?;
+        // Start request
+        .post_async("/match/:token/:fragment_number/start", |_req, ctx| async move {
+            // Store "START" fragment on R2
+            if let Some(token) = ctx.param("token") {
+                let namespace = ctx.durable_object(token)?;
+                let stub = namespace.id_from_name(token)?.get_stub()?;
+                // TODO: Upload fragment
+                // TODO: Register Match into Durable Objects
+            }
+            Response::error("Bad Request", 400)
+        })
 
-            Response::empty()
+        // Request from CS:GO Client
+        .get_async("/match/:token/sync", |_req, ctx| async move {
+            // Return sync JSON on DurableObjects
+            if let Some(token) = ctx.param("token") {
+                let namespace = ctx.durable_object("matches")?;
+                let stub = namespace.id_from_name(token)?.get_stub()?;
+                let resp = stub.fetch_with_str(token).await?.json::<Sync>().await?;
+                return Response::from_json(&resp)
+            }
+
+            Response::error("Bad Request", 400)
         })
         .run(req, env).await
 }
